e2af3b1d28298a209ae15cfc99b00da4f7c8e799
Taha Hassan <tahafhassan@gmai.com>
2022-10-06 17:39:53 +0100

    msg when over the limit, removed console.log

--- commentBox.js
+++ commentBox.js
@@ -4,10 +4,9 @@ function myFunction() {
     const comment = document.getElementById("comment");
     const commentLength = comment.value.length;
     const tracker = document.getElementById("tracker");
-    console.log(commentLength);
     tracker.innerHTML = "(" + commentLength.toString() + "/140)"
     if (commentLength > 140) {
-
+        tracker.innerHTML += " Woah! You're over the limit buddy!"
     } else {
 
     }
