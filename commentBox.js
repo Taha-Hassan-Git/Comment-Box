058bb4a28dc04808819200ae217c29c1dd9c0b19
Taha Hassan <tahafhassan@gmai.com>
2022-10-08 17:37:00 +0100

    rem warnings when char count was reduced under 140

--- commentBox.js
+++ commentBox.js
@@ -5,8 +5,11 @@ function myFunction() {
     const commentLength = comment.value.length;
     const tracker = document.getElementById("tracker");
     tracker.innerHTML = "(" + commentLength.toString() + "/140)"
+    tracker.style.color = "black"
+    comment.style.background = "white"
     if (commentLength > 140) {
         tracker.innerHTML += "<br> Woah! You're over the limit buddy!"
+        tracker.style.color = "red"
         comment.style.background = "rgba(245, 149, 149, 0.5)"
     } else {
 
